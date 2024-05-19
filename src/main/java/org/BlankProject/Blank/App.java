d18d5aad871e56153ca75fb2da91cb1fb37d9498
apsnad123 <aparnanandanshiat@gmail.com>
2024-05-19 10:56:25 +0530

    FirstCommit

---
+++ src/main/java/org/BlankProject/Blank/App.java
@@ -0,0 +1,13 @@
+package org.BlankProject.Blank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
